1fa76aaee3f62e0fa84b8b82816cda8b8ab8ad44
Nikoleto Scripts <151961875+nikoladhima@users.noreply.github.com>
2025-12-06 23:23:26 +0200

    Update DefuseDivisionSkinChanger.lua

--- utils/DefuseDivisionSkinChanger.lua
+++ utils/DefuseDivisionSkinChanger.lua
@@ -1,8 +1,1 @@
-local genv = getgenv or function()
-  return shared
-end
-if genv().ns__DefuseDivisionSkinChanger == "ns__loaded" then
-  return
-end
--- l
-genv().ns__DefuseDivisionSkinChanger = "ns__loaded"
+--

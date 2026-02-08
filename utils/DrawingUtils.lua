9aeab7b8ff0633a98c04ea6a4da35113efeab39a
Nikoleto Scripts <151961875+nikoladhima@users.noreply.github.com>
2026-02-08 15:00:43 +0200

    Create DrawingUtils.lua

---
+++ utils/DrawingUtils.lua
@@ -0,0 +1,72 @@
+local DrawingUtilities = {}
+DrawingUtilities.__index = DrawingUtilities
+
+function DrawingUtilities.new(DrawingObject)
+    return setmetatable({
+        DrawingObject = DrawingObject
+    }, DrawingUtilities)
+end
+
+DrawingUtilities.__index = function(self, Key)
+    return DrawingUtilities[Key] or self.DrawingObject[Key]
+end
+
+DrawingUtilities.__newindex = function(self, Key, Value)
+    self.DrawingObject[Key] = Value
+end
+
+for _,Property in ipairs({"Visible", "Center", "Size", "Color", "Filled", "Thickness", "Transparency", "Radius", "Text"}) do
+    DrawingUtilities[Property] = function(self, Value)
+        if self.DrawingObject[Property] ~= Value then
+            self.DrawingObject[Property] = Value
+        end
+        return self
+    end
+end
+
+for _,Property in ipairs({"Position", "From", "To", "PointA", "PointB", "PointC"}) do
+    DrawingUtilities[Property] = function(self, Value)
+        self.DrawingObject[Property] = Value
+        return self
+    end
+end
+
+function DrawingUtilities:Nil()
+    local Success = pcall(function()
+        self.DrawingObject:Remove()
+    end)
+    return Success
+end
+
+function DrawingUtilities.HideAll(ESP)
+    if not ESP then
+        return
+    end
+
+    for Index, Object in next, ESP do
+        if Index == "Cham" or Index == "Profile" then
+            if Object.Enabled ~= false then
+                Object.Enabled = false
+            end
+            continue
+        end
+
+        if Index == "Box3D" or Index == "Skeleton" then
+            for _,Line in next, Index do
+                Line:Visible(false)
+            end
+            continue
+        end
+
+        Object:Visible(false)
+    end
+end
+
+function DrawingUtilities.GetDistanceSquared(Point1, Point2)
+    local DeltaX = Point1.X - Point2.X
+    local DeltaY = Point1.Y - Point2.Y
+    local DeltaZ = Point1.Z - Point2.Z
+    return DeltaX * DeltaX + DeltaY * DeltaY + DeltaZ * DeltaZ
+end
+
+return DrawingUtilities
